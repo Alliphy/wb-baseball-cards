d850ec4dea5acfbf8f00ba749ca420eab2ac3923
Alliphy <ruebaker29@gmail.com>
2024-05-28 20:40:07 -0600

    upload to git

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import playerData from "./playerData.js"
+
+
+
+function BaseballCard(props) {
+  // console.log(props)
+  // return (
+  //   <div className="card">
+  //     <h2>{props.name}</h2>
+  //     <img src={props.imgUrl} alt={props.name} />
+  //   </div>
+// );
+
+const statsDisplay =
+
+Object.entries(props.stats).map(([statName, statValue]) => (
+    <p key={statName}>
+      {statName}: {statValue}
+    </p>
+));
+
+return (
+  <div className="card">
+    <h2>{props.name}</h2>
+    <p>Team: {props.team}</p>
+    <p>Position: {props.position}</p>
+    {statsDisplay}
+  </div>
+);
+}
+
+
+function App() {
+  let cards = playerData.map( (player) => {
+    return <BaseballCard
+    name={player.name}
+    team={player.team}
+    position={player.position}
+    stats={player.stats}
+    imgUrl={player.imgUrl}
+    key={player.cardId}
+
+    />
+  });
+  return <>{cards}</>
+}
+
+export default App;
